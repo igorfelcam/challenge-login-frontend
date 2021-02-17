31def753ff5a4211f46f7e623e12083df083c6c7
Igor Felipe de Camargo <igor.camargo@cwi.com.br>
2021-02-17 00:42:34 -0300

    feat/start challenger

---
+++ login/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Login from './pages/Login/'
+// import logo from './wiser-banner.jpg';
+
+function App() {
+  return (
+
+    <>
+
+      {/* <img src={logo} alt="logo" /> */}
+      <Login />
+
+    </>
+
+  );
+}
+
+export default App;
---
+++ login/src/pages/Login/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Login() {
+
+  return (
+    <>
+
+      <h1>
+        Hello Wiser
+      </h1>
+
+    </>
+  );
+
+}
+
+export default Login;

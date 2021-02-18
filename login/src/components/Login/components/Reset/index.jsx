b2d4754c306ba2dcc52882f0208a7b26e46470d1
Igor Felipe de Camargo <igor.camargo@cwi.com.br>
2021-02-18 12:13:04 -0300

    fix/div dependency p

--- login/src/components/Login/components/Reset/index.jsx
+++ login/src/components/Login/components/Reset/index.jsx
@@ -6,17 +6,15 @@ export const Reset = () => {
 
   return (
     <>
-
       <div className="reset">
         
-        <p>Esqueceu seu login ou senha?
-          <div>
-            Clique <a className="reset__link" href="https://wisereducacao.com/">aqui</a>
-          </div>
+        <p>Esqueceu seu login ou senha?</p>
+
+        <p>
+          Clique <a className="reset__link" href="https://wisereducacao.com/">aqui</a>
         </p>
 
      </div>
-
     </>
   );
 
